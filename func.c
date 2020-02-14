aac3eb000444a68dcf597731523bdbe3e876cb5e
fly-fisher <18922212390@189.cn>
2020-02-14 11:13:44 +0800

    master add func2

--- func.c
+++ func.c
@@ -8,8 +8,14 @@ void func_feature1()
   printf("feature1\n");
 }
 
+void func_feature2()
+{
+ printf("feature2\n");
+}
+
 int main(int argc, char *argv[])
 {
   func_feature1();
+  func_feature2();
   return 0;
 }
